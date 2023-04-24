db3d8e6d50ba92e4414891fa8b305841ce900201
anowar-dev <anowerhossan1988@gmail.com>
2023-04-24 23:50:30 +0600

    Checkout page done

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//=========== This section for mobile menu ============ 
+
 function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
@@ -6,3 +8,17 @@ function myFunction() {
       x.style.display = "block";
     }
   }
+
+//=========== This section for Back to top btn ============ 
+let mybutton = document.getElementById("topBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
