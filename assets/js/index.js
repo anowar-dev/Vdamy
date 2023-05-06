16e4b9c323c5777f5d7a9da2d65267a12967461f
Md. Anower Hossan <anowerhossan1988@gmail.com>
2023-05-06 15:28:12 +0600

    Video player control done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,20 @@
-//=========== This section for mobile menu ============ 
+//=========== This section for mobile menu ============
 
 function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
+}
 
-//=========== This section for Back to top btn ============ 
+//=========== This section for Back to top btn ============
 let mybutton = document.getElementById("topBtn");
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -23,31 +25,30 @@ function scrollFunction() {
 }
 
 //================== Owl Carousel for About bottom Js=================
-$('.owl-carousel').owlCarousel({
+$(".owl-carousel").owlCarousel({
   loop: true,
   margin: 20,
-  nav:  false,
+  nav: false,
   dots: true,
   autoplay: true,
   autoplayTimeout: 5000,
-  responsive:{
-    0:{
-      items:1
+  responsive: {
+    0: {
+      items: 1,
     },
-    600:{
-      items:1
+    600: {
+      items: 1,
     },
-    1000:{
-      items:2
-    }
-  }
-})
-
+    1000: {
+      items: 2,
+    },
+  },
+});
 
 //================== Slick Slider Js=================
 
-$(document).ready(function(){
-  $('.single-item').slick({
+$(document).ready(function () {
+  $(".single-item").slick({
     autoplay: true,
     autoplayTimeout: 2000,
     dots: true,
@@ -55,17 +56,30 @@ $(document).ready(function(){
   });
 });
 
-
 //============= Inner Video===============
-var btn = document.getElementById("preview");
-var popUp = document.getElementById("video_popup");
+// 1. manually src set on button and get the src when popup btn click
+// --------------------------------------------------------------------
 
-var close = document.getElementsByClassName("video_close")[0];
+// Gets the video src from the data-src on each button
+var $videoSrc;
+const popup_open_btn = document.getElementById("popup_open_btn");
+popup_open_btn.addEventListener("click", function (event) {
+  $videoSrc = $(this).data("src");
+  console.log($videoSrc);
+});
 
-  btn.onclick = function(){
-    popUp.style.display = "block";
-  }
+// when the modal is opened autoplay it
+const video_modal = document.getElementById("video_modal");
+const iframeID = document.getElementById("iframID");
 
-  close.onclick = function(){
-    popUp.style.display = "none";
-  }
+video_modal.addEventListener("shown.bs.modal", function (event) {
+  iframeID.setAttribute(
+    "src",
+    $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0"
+  );
+});
+
+// stop playing the youtube video when I close the modal
+video_modal.addEventListener("hide.bs.modal", function (event) {
+  iframeID.setAttribute("src", $videoSrc);
+});
